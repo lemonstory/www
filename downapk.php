112c400724762109ae0332cad92974e01ec294bd
huqiangqiang <will8436992@sina.com>
2015-11-14 15:27:07 +0800

    edit downapk

--- downapk.php
+++ downapk.php
@@ -1,5 +1,13 @@
 <?php
-$filename  = "xnm_1.0.0.apk";
-
-$downurl =  JICDOMAIN . "/apk/{$filename}";
-header("Location:".$downurl);
+include_once './controller.php';
+class downapk extends controller
+{
+    public function action()
+    {
+        $filename  = "xnm_1.0.0.apk";
+        
+        $downurl =  JICDOMAIN . "/apk/{$filename}";
+        header("Location:".$downurl);
+    }
+}
+new downapk();
