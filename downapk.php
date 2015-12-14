923ce7d149b484cf968903184324a19248314b7c
gaoyong <gaoyong06@qq.com>
2015-12-14 13:08:36 +0800

    增加:新版本1.2

--- downapk.php
+++ downapk.php
@@ -4,7 +4,7 @@ class downapk extends controller
 {
     public function action()
     {
-        $filename  = "xnm_1.1.apk";
+        $filename  = "xnm_1.2.apk";
         
         $downurl =  JICDOMAIN . "/apk/{$filename}";
         header("Location:".$downurl);
