b96faa4e70805a9bcae413e09e07ef5318b80e2f
Lyndon1994 <wuhulinyi@sina.com>
2016-12-09 18:20:40 +0800

    修改作者百科

--- author/detail.php
+++ author/detail.php
@@ -10,9 +10,30 @@ class authorDetail extends controller
 {
     public function action()
     {
-
+        $uid = $this->getRequest('uid');
+        if (empty($uid)) {
+            header("HTTP/1.0 404 Not Found");
+            exit;
+        }
+        $userobj = new User();
+        $alioss = new AliOss();
+        $userinfo = current($userobj->getUserInfo($uid, 1));
+        if (empty($userinfo)) {
+            header("HTTP/1.0 404 Not Found");
+            exit;
+        }
+        if (!empty($userinfo['avatartime'])) {
+            $userinfo['avatar'] = $alioss->getAvatarUrl($uid, $userinfo['avatartime'], 80);
+        }
+        $creator = new Creator();
+        $creatorInfo = $creator->getCreatorInfo($uid);
+        if (empty($creatorInfo)) {
+            header("HTTP/1.0 404 Not Found");
+            exit;
+        }
         $smartyObj = $this->getSmartyObj();
-        
+        $smartyObj->assign('userinfo', $userinfo);
+        $smartyObj->assign('creatorInfo', $creatorInfo);
         $smartyObj->display("author/detail.html");
     }
 
